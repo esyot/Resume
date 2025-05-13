ec68f24017c0af122a702dccc145e94e7d791b25
Esteban-ExpenseTracker <rei.nhard3367@gmail.com>
2025-05-13 19:44:58 +0800

    updated codes -main

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
@@ -11,6 +12,6 @@ export default defineConfig({
     },
   },
   server: {
-    historyApiFallback: true,
+    historyApiFallback: true, // Ensure all requests fall back to index.html
   },
 });
