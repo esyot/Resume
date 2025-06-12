6402c2c57f108d21cc5774461ebe8eecbb94adb1
Esteban-ExpenseTracker <rei.nhard3367@gmail.com>
2025-06-12 16:52:18 +0800

    updated codes -main

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -7,29 +7,29 @@ const toast = useToast();
 
 const contactInfo = [
   {
-    icon: "freecon icon-envelope text-grayy-400 text-xl w-6",
+    icon: "freecon icon-envelope icon-gray-400",
     text: "rei.nhard3367@gmail.com",
     isLink: false,
   },
   {
-    icon: "freecon icon-phone text-green-400 text-xl w-6",
+    icon: "freecon icon-phone icon-green-400",
     text: "+63 (966) 867-4260 (Globe)",
     isLink: false,
   },
   {
-    icon: "freecon brand-facebook text-blue-500 text-xl w-6",
+    icon: "freecon brand-facebook icon-blue-500",
     text: "/reinhard.esteban",
     link: "https://facebook.com/reinhard.esteban",
     isLink: true,
   },
   {
-    icon: "freecon brand-instagram text-pink-400 text-xl w-6",
+    icon: "freecon brand-instagram icon-pink-400",
     text: "@esyyyot",
     link: "https://instagram.com/esyyyot",
     isLink: true,
   },
   {
-    icon: "freecon brand-linkedin text-blue-400 text-xl w-6",
+    icon: "freecon brand-linkedin icon-blue-400",
     text: "/in/reinhard-esteban",
     link: "https://linkedin.com/in/reinhard-esteban",
     isLink: true,
@@ -47,14 +47,14 @@ const copyToClipboard = (text) => {
   <DefaultLayout>
     <div class="min-h-screen flex items-center justify-center bg-black/90">
       <div class="m-4 bg-yellow-500/20 rounded-2xl shadow-md w-full max-w-xl">
-        <ul class="space-y-4 text-yellow-100 text-lg p-6 m-2">
+        <ul class="space-y-4 text-lg p-6 m-2">
           <li
             v-for="(item, index) in contactInfo"
             :key="index"
             class="flex items-center space-x-4"
           >
             <i :class="item.icon"></i>
-            <span v-if="!item.isLink">{{ item.text }}</span>
+            <span v-if="!item.isLink" class="text-yellow-400">{{ item.text }}</span>
             <a
               v-else
               :href="item.link"
@@ -67,7 +67,7 @@ const copyToClipboard = (text) => {
               class="ml-auto text-gray-200 hover:text-white"
               @click="copyToClipboard(item.text)"
             >
-              <i class="freecon fa-copy"></i>
+              <i class="freecon icon-copy icon-hover-fade"></i>
             </button>
           </li>
         </ul>
